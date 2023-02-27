e2d78a47a6584e77401f8655cc3a26efa3f6d460
RUWAIM <mhruwaim@gmail.com>
2023-02-27 15:36:51 +0530

    first commit

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,11 +10,12 @@ items=[
   {
     img :"../../assets/61WFNqf8hVL._SL1500_.jpg"
   },{
-    img :"../../assets/61WFNqf8hVL._SL1500_.jpg"
+    img :"../../assets/boAt-BassHeads-110-Earphones-491431549-i-1-1200Wx1200H (1).jpeg"
   },{
-    img :"../../assets/61WFNqf8hVL._SL1500_.jpg"
+    img :"../../../assets/in-galaxy-buds2-pro-r510-sm-r510nzaainu-533192316.avif"
   },{
-    img :"../../assets/61WFNqf8hVL._SL1500_.jpg"
+    img :"../../assets/Arrow-A11-Earphones-492579558-i-1-1200Wx1200H (1).jpeg" 
+
   },{
     img :"../../assets/61WFNqf8hVL._SL1500_.jpg"
   },
